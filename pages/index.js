817b22b2fd731c5c7f1d2ef992f02c2a519f8de8
Abhishek7tech <abhishektab45@gmail.com>
2022-10-16 18:24:59 +0530

    updated videos search queries

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,28 @@ import { Banner } from "../components/banner/banner";
 import { Navbar } from "../components/nav/navBar";
 import styles from "../styles/Home.module.css";
 import { SectionCards } from "../components/card/section-cards";
-import { getVideos } from "../lib/videos";
-export default function Home() {
- const videos = getVideos();
+import { getPopularVideos, getVideos } from "../lib/videos";
+
+//ServerSide Rendering//
+export async function getServerSideProps() {
+  const animeVideos = await getVideos("anime trailer");
+  const documentryVideos = await getVideos("netflix documentry trailer");
+  const travelVideos = await getVideos("travel videos");
+  const popularVideos = await getPopularVideos();
+return {
+  props: {animeVideos,documentryVideos,travelVideos,popularVideos}
+}
+}
+
+export default function Home(props) {
+  const {animeVideos, documentryVideos, travelVideos, popularVideos} = props;
   return (
     <div className={styles.container}>
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <div className={styles.main}>
       <Navbar userName={"Abhishektab45@gmail.com"} />
       <Banner
         title="Stranger Things"
@@ -21,11 +33,13 @@ export default function Home() {
         imgUrl="/static/Stranger.webp"
       />
       <div className={styles.sectionWrapper}>
-      <SectionCards title="Disney" videos={videos} size="large"/>
-      <SectionCards title="Disney" videos={videos} size="medium"/>
+      <SectionCards title="Anime" videos={animeVideos} size="large"/>
+      <SectionCards title="Documentries" videos={documentryVideos} size="medium"/>
+      <SectionCards title="Popular" videos={popularVideos} size="small"/>
+      <SectionCards title="Travel" videos={travelVideos} size="medium"/>
       
       </div>
-     
+      </div>
     </div>
   );
 }
